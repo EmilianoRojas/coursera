ca6e538526ea10be10e90168f227e36d028f5818
EmilianoRojas <inversiones.socony@gmail.com>
2020-07-11 15:41:34 -0400

    contact form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,27 @@
 import React, { Component, Fragment } from 'react'
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom'
 
 export default class HeaderComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      isModalOpen: false
     }
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen
+    })
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
     })
   }
 
@@ -49,6 +57,13 @@ export default class HeaderComponent extends Component {
                 </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className='ml-auto' navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className='fa fa-sign-in fa-lg'></span> Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -62,6 +77,14 @@ export default class HeaderComponent extends Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>
+            Login
+          </ModalHeader>
+          <ModalBody>
+
+          </ModalBody>
+        </Modal>
       </Fragment>
     )
   }
